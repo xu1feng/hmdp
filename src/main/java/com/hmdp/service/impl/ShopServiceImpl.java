315ba9cf95eb4a4547b63e662a481d0112619cb1
xu1feng <2465549609@qq.com>
2024-11-14 19:43:55 +0800

    feat: 利用互斥锁方式解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -31,20 +32,88 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
+        // 缓存穿透
+//        Shop shop = queryWithPassThrough(id);
+
+        // 互斥锁解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在！");
+        }
+        return Result.ok(shop);
+    }
+
+    private Shop queryWithMutex(Long id) {
         // 1.从redis查询商铺缓存
         String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
 
         // 2.判断是否存在
         if (StrUtil.isNotBlank(shopJson)) {
             // 3.存在，直接返回
-            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return JSONUtil.toBean(shopJson, Shop.class);
         }
 
         // 判断命中的是否是空值
         if (shopJson != null) {
             // 返回一个错误信息
-            return Result.fail("店铺信息不存在！");
+            return null;
+        }
+
+        // 4.开始实现缓存重建
+        // 4.1获取互斥锁
+        String lockKey = "lock:shop:" + id;
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+
+            // 4.2判断是否获取成功
+            if (!isLock) {
+                // 4.3失败，休眠并重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+
+            // 4.4成功，根据id查询数据库
+            shop = getById(id);
+
+            // 模拟重建延时
+            Thread.sleep(200);
+
+            // 5.不存在，返回错误
+            if (shop == null) {
+                // 将空值写入redis
+                stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                // 返回错误信息
+                return null;
+            }
+
+            // 6.存在，写入redis
+            stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 7.释放互斥锁
+            unLock(lockKey);
+        }
+
+        // 8.返回
+        return shop;
+    }
+
+    private Shop queryWithPassThrough(Long id) {
+        // 1.从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
+
+        // 2.判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3.存在，直接返回
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+
+        // 判断命中的是否是空值
+        if (shopJson != null) {
+            // 返回一个错误信息
+            return null;
         }
 
         // 4.不存在，根据id查询数据库
@@ -55,14 +124,23 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             // 将空值写入redis
             stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
             // 返回错误信息
-            return Result.fail("店铺不存在！");
+            return null;
         }
 
         // 6.存在，写入redis
         stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         // 7.返回
-        return Result.ok(shop);
+        return shop;
+    }
+
+    private boolean tryLock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unLock(String key) {
+        stringRedisTemplate.delete(key);
     }
 
     @Override
