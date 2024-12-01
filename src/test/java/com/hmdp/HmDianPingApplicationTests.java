bdd60ecdbc3fe7251c11e84c5e8511b8b7cf3e55
xu1feng <2465549609@qq.com>
2024-12-01 20:48:33 +0800

    feat: 实现UV统计

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -90,4 +90,25 @@ class HmDianPingApplicationTests {
             stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
+
+    @Test
+    void testHyperLogLog() {
+        // 准备数组，装用户数据
+        String[] users = new String[1000];
+        // 数组角标
+        int index = 0;
+        for (int i = 1; i <= 1000000; i++) {
+            // 赋值
+            users[index++] = "user_" + i;
+            // 每1000条发送一次
+            if (i % 1000 == 0) {
+                index = 0;
+                stringRedisTemplate.opsForHyperLogLog().add("hll1", users);
+            }
+        }
+        // 统计数量
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hll1");
+        System.out.println("size = " + size);
+    }
+
 }
